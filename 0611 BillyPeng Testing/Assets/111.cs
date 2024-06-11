c358eeb81005750d473c7afd391a98fd2fbf87f1
袁佳翔 <157483374+JiaxiangYuan@users.noreply.github.com>
2024-06-11 19:16:21 +0800

    test test

---
+++ 0611 BillyPeng Testing/Assets/111.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class utyvib : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("阴暗地爬行）（尖叫） （爬行）（扭动）（分裂）（阴暗地蠕动）（翻滚）（激烈地爬动）（扭曲）（痉挛）（嘶吼）（蠕动）（阴森地低吼）（爬行）（分裂）（走上岸）（扭动）（痉挛）（蠕动）（扭曲地行走）");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
